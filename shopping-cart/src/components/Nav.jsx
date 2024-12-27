e63d29b1fcce105f2aed2110144e64ad9f3d94c8
Estel <jiankangzhao7@gmail.com>
2024-12-28 01:58:33 +0800

    complete basic setup

---
+++ shopping-cart/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+export function Nav() {
+  return (
+    <>
+      <Link to="/home">Home</Link>
+      <div></div>
+      <Link to="Shopping">Shopping</Link>
+    </>
+  );
+}

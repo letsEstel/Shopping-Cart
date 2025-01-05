41d5ebfdfd9874cb6cd32b05b6a757c328baa852
Estel <jiankangzhao7@gmail.com>
2025-01-06 00:58:30 +0800

    fix product display

--- shopping-cart/src/components/Nav.jsx
+++ shopping-cart/src/components/Nav.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router";
 export function Nav() {
   return (
     <header>
-      <Link to="/">Home</Link>
-      <Link to="/Mall">Mall</Link>
-      <Link to="/Mall/Cart">Cart</Link>
+      <p>Shopping Cart</p>
+      <div className="linkList">
+        <Link to="/">Home</Link>
+        <Link to="/Mall">Mall</Link>
+        <Link to="/Mall/Cart">Cart</Link>
+      </div>
     </header>
   );
 }
